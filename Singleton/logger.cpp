e530c092e6818a01d6a14173229ef0997e54bf56
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-10-09 10:24:20 +0530

    Demonstrating that if multithreading is being used you can create two instance of the singleton class

--- Singleton/logger.cpp
+++ Singleton/logger.cpp
@@ -18,6 +18,7 @@ void Logger::Log(string msg)
 
 Logger *Logger::getLogger()
 {
+
     if (loggerInstance == nullptr)
     {
         loggerInstance = new Logger();
--- Singleton/user.cpp
+++ Singleton/user.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 #include "logger.hpp"
+#include <thread>
 using namespace std;
 
-int main()
+Logger *createLogger1()
 {
     Logger *logger1 = Logger::getLogger();
     logger1->Log("this msg is from user 1");
+    return logger1;
+}
 
+Logger *createLogger2()
+{
     Logger *logger2 = Logger::getLogger();
     logger2->Log("this msg is from user 2");
+    return logger2;
+}
+int main()
+{
+
+    thread t1(createLogger1);
+    thread t2(createLogger2);
+    t1.join();
+    t2.join();
     return 0;
 }
